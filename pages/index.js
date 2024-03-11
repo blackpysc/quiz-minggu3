57daee0e9a8a2a16f498485a271c99e45bc9b2fc
blackpysc <ext404@gmail.com>
2024-03-12 01:13:36 +0700

    tugas-hari-ke9

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response =>", res))
+      .catch((err) => console.log("error =>", err));
+  }, []);
   return (
     <>
       <Layout metaTitle="Home" metaDescription="halaman home">
