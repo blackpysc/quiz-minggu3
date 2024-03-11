dda01458b72c47e281012f4f756071b3d76c8609
blackpysc <ext404@gmail.com>
2024-03-11 23:56:51 +0700

    tugas-hari-ke7

---
+++ pages/users/detail.js
@@ -0,0 +1,9 @@
+import Layout from "@/layout";
+
+export default function Detail() {
+    return (
+        <Layout metaTitle="user detail">
+            <p>Users Details</p>
+        </Layout>
+    )
+}
---
+++ pages/users/index.js
@@ -0,0 +1,9 @@
+import Layout from "@/layout";
+
+export default function Users(){
+    return (
+        <Layout metaTitle="users" >
+            <p>Users</p>
+        </Layout>
+    )
+}
